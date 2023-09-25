c0cbba7864267c48c1c1929eb872fa4f1fe2fab6
zSnails <erizojuan33@gmail.com>
2023-09-24 20:07:26 -0600

    Add bonus implementation

--- practica4/sumlist.pl
+++ practica4/sumlist.pl
@@ -4,3 +4,9 @@ sumlist([], 0).
 sumlist([H|T], S) :-
     sumlist(T, K),
     S is H + K.
+
+% NOTE: Esta es una implementación bonus que se me ocurrió, suelta una
+% expresión donde el resultado es la suma de todos los demás.
+% sumlist([], 0).
+% sumlist([H|T], H + K) :-
+%     sumlist(T, K).
